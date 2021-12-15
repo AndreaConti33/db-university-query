e8b6d394e0055a77f74b04a0cb5d61421f7a49a1
AndreaConti <andrea.conti23331@gmail.com>
2021-12-15 16:48:13 +0100

    Consegna (bonus da controllare)

---
+++ db_university_query.sql
@@ -0,0 +1,53 @@
+use db_university_query;
+
+-- Selezionare tutti gli studenti nati nel 1990 
+select * from students s where year(date_of_birth) = 1990;
+
+-- Selezionare tutti i corsi che valgono più di 10 crediti
+select * from courses c where cfu > 10;
+
+-- Selezionare tutti gli studenti che hanno più di 30 anni
+select * from students s where year(date_of_birth) > 1991;
+
+-- Selezionare tutti i corsi di laurea magistrale
+select * from degrees d where `level` = 'magistrale';
+
+-- Da quanti dipartimenti è composta l’università?
+select count(id) from departments d;
+
+-- Quanti sono gli insegnanti che non hanno un numero di telefono?
+select count(id) from teachers t where phone is NULL;
+
+-- Contare quanti iscritti ci sono stati ogni anno
+select count(id), year(enrolment_date) from students s 
+group by year(enrolment_date) order by year(enrolment_date) desc;
+
+-- Calcolare la media dei voti di ogni appello d’esame
+select exam_id , avg(vote) from exam_student es group by exam_id;
+
+-- Contare quanti corsi di laurea ci sono per ogni dipartimento
+select department_id, count(id) from degrees d group by department_id;
+
+-- Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+select * from students s where degree_id = 53; 
+
+-- Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
+select * from degrees d where department_id = 7;
+
+-- Selezionare tutti i corsi in cui insegna Fulvio Amato
+select * from course_teacher ct where teacher_id = 44
+
+-- Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+select s.name, s.surname, d.name as Corso, d2.name as Dipartimento from students s inner join `degrees` d ON s.degree_id = d.id inner join departments d2 on d.department_id = d2.id;
+
+-- BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+select  s.id, s.surname, s.name, count(es.exam_id) as esami_svolti from exam_student es 
+inner join students s ON es.student_id = s.id
+inner join exams e ON es.exam_id = e.id
+group by es.exam_id order by s.surname, s.name;
+
+
+
+
+
+
